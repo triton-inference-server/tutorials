442a7a412aafc7c996299af58b49d3d8f955474c
tanayvarshney <tvarshney@nvidia.com>
2023-01-11 22:04:06 -0800

    added huggingface example

---
+++ HuggingFace/ensemble_model_repository/preprocessing/1/model.py
@@ -0,0 +1,31 @@
+import json
+import numpy as np
+import torch
+import triton_python_backend_utils as pb_utils
+
+import requests
+from transformers import ViTFeatureExtractor
+
+
+class TritonPythonModel:
+
+    def initialize(self, args):
+        self.feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224-in21k')
+
+    def execute(self, requests):
+        responses = []
+        for request in requests:
+            inp = pb_utils.get_input_tensor_by_name(request, "image")
+            input_image = np.squeeze(inp.as_numpy()).transpose((2,0,1))
+
+            inputs = self.feature_extractor(images=input_image, return_tensors="pt")
+            pixel_values = inputs['pixel_values'].numpy()
+
+            inference_response = pb_utils.InferenceResponse(output_tensors=[
+                pb_utils.Tensor(
+                    "pixel_values",
+                    pixel_values,
+                )
+            ])
+            responses.append(inference_response)
+        return responses
---
+++ HuggingFace/python_model_repository/python_vit/1/model.py
@@ -0,0 +1,30 @@
+import json
+import numpy as np
+import triton_python_backend_utils as pb_utils
+
+from PIL import Image
+import requests
+from transformers import ViTFeatureExtractor, ViTModel
+
+class TritonPythonModel:
+    def initialize(self, args):
+        self.feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224-in21k')
+        self.model = ViTModel.from_pretrained("google/vit-base-patch16-224-in21k")
+
+    def execute(self, requests):
+        responses = []
+        for request in requests:
+            inp = pb_utils.get_input_tensor_by_name(request, "image")
+            input_image = np.squeeze(inp.as_numpy()).transpose((2,0,1))
+            inputs = self.feature_extractor(images=input_image, return_tensors="pt")
+
+            outputs = self.model(**inputs)
+
+            inference_response = pb_utils.InferenceResponse(output_tensors=[
+                pb_utils.Tensor(
+                    "label",
+                    outputs.last_hidden_state.numpy()
+                )
+            ])
+            responses.append(inference_response)
+        return responses
