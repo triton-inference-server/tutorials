5d70fe7e0ceea19a767d4182ee861b260e2c79c2
dyastremsky <58150256+dyastremsky@users.noreply.github.com>
2023-09-29 12:50:08 -0700

    Support input and parameter approaches for vLLM tutorial (#52)
    * Resolve unsupported InferenceRequest parameters in BLS (#50)
    
    Co-authored-by: tonywang10101 <tony.wnag.10101@gmail.com>
    
    * Allow both input and parameter approach for vLLM (#51)
    
    
    Co-authored-by: tonywang10101 <tony.wnag.10101@gmail.com>
    Co-authored-by: nnshah1 <neelays@nvidia.com>
    
    ---------
    
    Co-authored-by: tonywang10101 <tony.wnag.10101@gmail.com>
    Co-authored-by: nnshah1 <neelays@nvidia.com>

--- Quick_Deploy/vLLM/client.py
+++ Quick_Deploy/vLLM/client.py
@@ -29,6 +29,7 @@ import asyncio
 import queue
 import sys
 from os import system
+import json
 
 import numpy as np
 import tritonclient.grpc.aio as grpcclient
@@ -40,7 +41,7 @@ class UserData:
         self._completed_requests = queue.Queue()
 
 
-def create_request(prompt, stream, request_id, sampling_parameters, model_name):
+def create_request(prompt, stream, request_id, sampling_parameters, model_name, send_parameters_as_tensor=True):
     inputs = []
     prompt_data = np.array([prompt.encode("utf-8")], dtype=np.object_)
     try:
@@ -53,6 +54,17 @@ def create_request(prompt, stream, request_id, sampling_parameters, model_name):
     inputs.append(grpcclient.InferInput("STREAM", [1], "BOOL"))
     inputs[-1].set_data_from_numpy(stream_data)
 
+    # Request parameters are not yet supported via BLS. Provide an
+    # optional mechanism to send serialized parameters as an input
+    # tensor until support is added
+    
+    if send_parameters_as_tensor:
+        sampling_parameters_data = np.array(
+            [json.dumps(sampling_parameters).encode("utf-8")], dtype=np.object_
+        )
+        inputs.append(grpcclient.InferInput("SAMPLING_PARAMETERS", [1], "BYTES"))
+        inputs[-1].set_data_from_numpy(sampling_parameters_data)
+
     # Add requested outputs
     outputs = []
     outputs.append(grpcclient.InferRequestedOutput("TEXT"))
@@ -63,7 +75,7 @@ def create_request(prompt, stream, request_id, sampling_parameters, model_name):
         "inputs": inputs,
         "outputs": outputs,
         "request_id": str(request_id),
-        "parameters": sampling_parameters,
+        "parameters": sampling_parameters
     }
 
 
