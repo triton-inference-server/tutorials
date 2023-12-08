a14e2f64910a5340bb78213cff4db97da6f3a8ee
nnshah1 <neelays@nvidia.com>
2023-12-08 06:43:08 -0800

    testing for decoupled mode

---
+++ RayServe/models_test/test_2/1/model.py
@@ -0,0 +1,44 @@
+import json
+
+import numpy as np
+import triton_python_backend_utils as pb_utils
+
+
+class TritonPythonModel:
+    def initialize(self, args):
+        self._model_config = json.loads(args["model_config"])
+        self._decoupled = self._model_config.get("model_transaction_policy", {}).get(
+            "decoupled"
+        )
+
+    def execute_decoupled(self, requests):
+        for request in requests:
+            sender = request.get_response_sender()
+            output_tensors = []
+            for input_tensor in request.inputs():
+                input_value = input_tensor.as_numpy()
+                output_tensor = pb_utils.Tensor(
+                    input_tensor.name().replace("input", "output"), input_value
+                )
+                output_tensors.append(output_tensor)
+            sender.send(pb_utils.InferenceResponse(output_tensors=output_tensors))
+            print("sending final!")
+            sender.send(flags=pb_utils.TRITONSERVER_RESPONSE_COMPLETE_FINAL)
+        return None
+
+    def execute(self, requests):
+        if self._decoupled:
+            return self.execute_decoupled(requests)
+
+        responses = []
+        for request in requests:
+            output_tensors = []
+            for input_tensor in request.inputs():
+                input_value = input_tensor.as_numpy()
+                output_tensor = pb_utils.Tensor(
+                    input_tensor.name().replace("input", "output"), input_value
+                )
+                output_tensors.append(output_tensor)
+
+            responses.append(pb_utils.InferenceResponse(output_tensors=output_tensors))
+        return responses
