c85e4443a3dac6393986c94f095c9dfe3a633818
nnshah1 <neelays@nvidia.com>
2023-12-15 17:25:34 -0800

    updating stable diffusion build script

--- RayServe/export_and_convert.sh
+++ RayServe/export_and_convert.sh
@@ -3,8 +3,8 @@ pip3 install -r requirements_model_export.txt
 huggingface-cli login --token $HF_TOKEN
 python export.py
 trtexec --onnx=vae.onnx --saveEngine=vae.plan --minShapes=latent_sample:1x4x64x64 --optShapes=latent_sample:4x4x64x64 --maxShapes=latent_sample:8x4x64x64 --fp16
-mkdir -p model_repository/vae/1
-mkdir -p model_repository/text_encoder/1
-mv vae.plan model_repository/vae/1/model.plan
-mv encoder.onnx model_repository/text_encoder/1/model.onnx
+mkdir -p models_stable_diffusion/vae/1
+mkdir -p models_stable_diffusion/text_encoder/1
+mv vae.plan models_stable_diffusion/vae/1/model.plan
+mv encoder.onnx models_stable_diffusion/text_encoder/1/model.onnx
 
