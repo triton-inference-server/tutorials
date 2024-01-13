539acca08d42254e86f3561d13076cd818e3a796
nnshah1 <neelays@nvidia.com>
2024-01-12 20:42:56 -0800

    undoing unrelated changes

--- Conceptual_Guide/Part_6-building_complex_pipelines/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --gpus all -it --rm --network host --shm-size=10G --ulimit memlock=-1 --ulimit stack=67108864 -e HF_TOKEN -v ${PWD}:/workspace -v${PWD}/model_repository:/workspace/models -w /workspace rayserve-triton
--- Conceptual_Guide/Part_6-building_complex_pipelines/run_model_export.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --pull missing --gpus all -it --rm --network host --shm-size=10G --ulimit memlock=-1 --ulimit stack=67108864 -v ${PWD}:/mount nvcr.io/nvidia/pytorch:23.11-py3
