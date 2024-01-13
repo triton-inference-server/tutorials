b3bd8c8cf312c7317eaefe8a85e0a430e6a46985
nnshah1 <neelays@nvidia.com>
2024-01-12 17:02:11 -0800

    removing outdated example

--- RayServe/build.sh
+++
@@ -1,4 +0,0 @@
-# cp -rf ~/python_beta_api/local-tritonbuild/install/lib/libtritonserver.so ./deps/
-# cp -rf ~/python_beta_api/local-tritonbuild/install/python/tritonserver-2.42.0.dev0-py3-none-any.whl ./deps/
-# cp -rf ~/python_beta_api/local-tritonbuild/install/backends ./deps/
-docker build -t rayserve-triton -f Dockerfile .
--- RayServe/run_trt_llm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --gpus all -it --rm --network host --shm-size=10G --ulimit memlock=-1 --ulimit stack=67108864 -v ${PWD}:/workspace -v ${PWD}/models_trt_llm:/workspace/models -w /workspace rayserve-triton-trt-llm
