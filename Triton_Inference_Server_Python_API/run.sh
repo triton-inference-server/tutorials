3556d99eac85c7a8dcb3637eed3b5b8b80a5682b
nnshah1 <neelays@nvidia.com>
2024-06-03 22:06:41 -0700

    update run to 24.05

--- Triton_Inference_Server_Python_API/run.sh
+++ Triton_Inference_Server_Python_API/run.sh
@@ -102,7 +102,7 @@ get_options() {
     fi
 
     if [ -z "$IMAGE" ]; then
-        IMAGE="triton-python-api:r24.04"
+        IMAGE="triton-python-api:r24.05"
 
 	if [[ $FRAMEWORK == "TRT_LLM" ]]; then
 	    IMAGE+="-trt-llm"
